4d40ae3aae8d7148d69a11da925a60d3a005ec39
chagall831117 <57396729+chagall831117@users.noreply.github.com>
2020-03-26 20:07:12 +0800

    Ru

---
+++ 3DGame/Assets/Script/Learn.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class Learn : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("嗨");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        print("無馱 歐拉");
+    }
+}
---
+++ 3DGame/Assets/Script/LearnMemberStatic.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class LearnMemberStatic : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print(Random.value);
+        Time.timeScale = 0.1f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        print(Random.Range(1,5100));
+    }
+}
