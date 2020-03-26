3a3e1efd1e922456268d45bfd8d6b52da69de53a
chagall831117 <57396729+chagall831117@users.noreply.github.com>
2020-03-26 21:22:56 +0800

    0922

--- 3DGame/Assets/Script/LearnMemberStatic.cs
+++ 3DGame/Assets/Script/LearnMemberStatic.cs
@@ -2,16 +2,21 @@
 
 public class LearnMemberStatic : MonoBehaviour
 {
+    public Camera cam;
     // Start is called before the first frame update
     void Start()
     {
         print(Random.value);
         Time.timeScale = 0.1f;
+        print(Mathf.PI);
+        print(Mathf.Abs(-5156151561f));
     }
 
     // Update is called once per frame
     void Update()
     {
         print(Random.Range(1,5100));
+        print("攝影機數量" + Camera.allCamerasCount);
+        print(cam.depth);
     }
 }
