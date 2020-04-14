9700ebd3696744405a58765d044637eda8d4111c
chagall831117 <57396729+chagall831117@users.noreply.github.com>
2020-04-14 21:34:04 +0800

    Transfrom

---
+++ 3DGame/Assets/Script/CameraRoll.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRoll : MonoBehaviour
+{
+    public Transform dragon;
+    [Header("追蹤速度")]
+    public float Speed = 50;
+    public void track()
+    {
+        Vector3 draPos = dragon.position;
+        draPos.y = 50;
+        draPos.z -= 10;
+        transform.position = Vector3.Lerp(transform.position, draPos, 0.3f * Time.deltaTime *Speed);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void LateUpdate()
+    {
+        track();
+    }
+}
---
+++ 3DGame/Assets/Script/Dragon.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class Dragon : MonoBehaviour
+{
+    public Joystick Joy;
+    [Header("移動速度"),Range(1,1000)]
+    public float speed = 30;
+    /// <summary>
+    /// 移動方法
+    /// </summary>
+    public void Move()
+    {
+        float v = Input.GetAxis("Vertical");
+        float h = Input.GetAxis("Horizontal");
+        transform.Translate(speed * Time.deltaTime * h, 0, speed * Time.deltaTime * v);
+        float JoyV = Joy.Vertical;
+        float JoyH = Joy.Horizontal;
+        transform.Translate(speed * Time.deltaTime * JoyH, 0, speed * Time.deltaTime * JoyV);
+
+        Vector3 pos = transform.position;
+        pos.x = Mathf.Clamp(pos.x, 30, 70);
+        transform.position = pos;
+    }
+    private void Update()
+    {
+        Move();
+    }
+}
