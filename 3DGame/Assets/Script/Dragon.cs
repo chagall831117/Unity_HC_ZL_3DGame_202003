dc982ce73ad9545a944766c96dfc2c245568dee8
chagall831117 <57396729+chagall831117@users.noreply.github.com>
2020-04-16 21:39:28 +0800

    FireBall

--- 3DGame/Assets/Script/Dragon.cs
+++ 3DGame/Assets/Script/Dragon.cs
@@ -1,10 +1,20 @@
 ﻿using UnityEngine;
+using System.Collections;
 
 public class Dragon : MonoBehaviour
 {
+    [Header("阿就虛擬搖桿 還想怎樣")]
     public Joystick Joy;
-    [Header("移動速度"),Range(1,1000)]
+    [Header("阿就移動速度 還想怎樣"),Range(1,1000)]
     public float speed = 30;
+    [Header("啊就火球 不然呢")]
+    public GameObject FireBall;
+    [Header("啊就冷卻時間 不然呢")]
+    public float Cd = 1;
+    private Animator Ani;
+    private float Timer;
+    public float DelayFire = 0.2f;
+    public float FireBallSpeed = 500;
     /// <summary>
     /// 移動方法
     /// </summary>
@@ -19,10 +29,35 @@ public class Dragon : MonoBehaviour
 
         Vector3 pos = transform.position;
         pos.x = Mathf.Clamp(pos.x, 30, 70);
+        pos.z = Mathf.Clamp(pos.z, 20, 30);
         transform.position = pos;
+    }
+    private void Start()
+    {
+        Ani = GetComponent<Animator>();
+    }
+    public void Attack()
+    {
+        Timer += Time.deltaTime;
+        if (Timer >= Cd)
+        {
+            Ani.SetTrigger("攻擊觸發");
+            Timer = 0;
+            StartCoroutine(DelayFireBall());
+        }
+    }
+    public IEnumerator DelayFireBall()
+    {
+        yield return new WaitForSeconds(DelayFire);
+        Vector3 PosFire = transform.position;
+        PosFire.z += 8;
+        GameObject Temp = Instantiate(FireBall, PosFire, Quaternion.identity);
+        Temp.GetComponent<Rigidbody>().AddForce(0, 0, FireBallSpeed);
+
     }
     private void Update()
     {
         Move();
+        Attack();
     }
 }
---
+++ 3DGame/Assets/Script/GameManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public Transform[] Terrain;
+    public float TerrainSpeed = 1.5f;
+    public void MoveTerrain()
+    {
+        //
+        //Terrain[0].Translate(0, 0, -TerrainSpeed * Time.deltaTime);
+        //Terrain[1].Translate(0, 0, -TerrainSpeed * Time.deltaTime);
+        //if (Terrain[0].position.z <= -100)
+            //Terrain[0].position = new Vector3(0, 0, Terrain[1].position.z + 100);
+        //if (Terrain[1].position.z <= -100)
+            //Terrain[1].position = new Vector3(0, 0, Terrain[0].position.z + 100);
+        for (int i = 0; i < Terrain.Length; i++)
+        {//如果地板.Z小於100
+            if (Terrain[i].position.z <= -100)
+            {//另一塊地板的前方100位置
+            Terrain[i].position =  new Vector3(0, 0, Terrain[1 - i].position.z + 100);
+            }//移動
+        Terrain[i].Translate(0, 0, -TerrainSpeed * Time.deltaTime);
+        }
+    }
+    private void FixedUpdate()
+    {
+        MoveTerrain();
+    }
+}
